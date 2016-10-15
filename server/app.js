e27fe02e100dce18690c4941016ff1b4afcb38d6
Lukas Udstuen <Lukas.Udstuen@gmail.com>
2016-10-15 14:59:14 -0500

    file upload and parse to JSON completed

--- server/app.js
+++ server/app.js
@@ -4,6 +4,10 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 var multer = require('multer');
 var app = express();
+var xlstojson = require("xls-to-json-lc");
+var xlsxtojson = require("xlsx-to-json-lc");
+
+
 app.use(bodyParser.json());
 var storage = multer.diskStorage({
   destination: function(req, file, cb){
@@ -25,15 +29,42 @@ var upload = multer({ //multer settings
               }).single('file');
 /** API Path that will accept uploaded files**/
 
-app.post('/upload', function(req, res){
-  upload(req, res, function(err){
-    if (err) {
-      res.json({error_code:1, err_desc:err});
-      return;
-    }
-    res.json({error_code:0, err_desc:null});
-  });
-});
+app.post('/upload', function(req, res) {
+    var exceltojson;
+    upload(req,res,function(err){
+        if(err){
+             res.json({error_code:1,err_desc:err});
+             return;
+        }
+        /** Multer gives us file info in req.file object */
+        if(!req.file){
+            res.json({error_code:1,err_desc:"No file passed"});
+            return;
+        }
+        /** Check the extension of the incoming file and
+         *  use the appropriate module
+         */
+        if(req.file.originalname.split('.')[req.file.originalname.split('.').length-1] === 'xlsx'){
+            exceltojson = xlsxtojson;
+        } else {
+            exceltojson = xlstojson;
+        }
+        try {
+            exceltojson({
+                input: req.file.path,
+                output: null, //since we don't need output.json
+                lowerCaseHeaders:true
+            }, function(err,result){
+                if(err) {
+                    return res.json({error_code:1,err_desc:err, data: null});
+                }
+                res.json({error_code:0,err_desc:null, data: result});
+            });
+        } catch (e){
+            res.json({error_code:1,err_desc:"Corupted excel file"});
+        }
+    })
+}); 
 
 //Static Page
 app.use(express.static('public'));
