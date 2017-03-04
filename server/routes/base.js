4d74b9afece1a3c003ab09258a7ebd46d4404183
Lukas Udstuen <lukas.udstuen@gmail.com>
2017-03-04 16:04:52 -0600

    need to upload new query

--- server/routes/base.js
+++ server/routes/base.js
@@ -5,16 +5,27 @@ var bodyParser = require('body-parser');
 var router = express.Router();
 var fs = require('fs-extra');
 var rimraf = require('rimraf');
+var pg = require('pg');
 
 var async = require('async');
+
+
+if(process.env.DATABASE_URL !== undefined) {
+     console.log('env connection string');
+     connectionString = process.env.DATABASE_URL;
+     pg.defaults.ssl = true;
+} else {
+    // running locally, use our local database instead
+    connectionString = 'postgres://localhost:5432/groc';
+}
+
+
 
 router.get('/', function (req, res){
   res.sendFile(path.resolve('public/views/index.html'));
 });
 
-
 router.get('/upload1view', function(req, res) {
-
   async.series([
     function(callback){
         // Deletes the uploads file direcotry. This executes whenever a new upload page 1 is loaded
@@ -43,45 +54,17 @@ router.get('/upload2view', function(req, res){
 });
 
 router.get('/success', function(req, res){
+
+  pg.connect(connectionString, function(err, client, done){
+    // This is the query that is creating all the problems. Will need to edit this. 
+    // client.query("CREATE TABLE supervaludeals (itemupc bigint, brand varchar, description varchar, pack integer, size varchar, mxdlwk varchar, mxdlamt float, mxdealprct decimal, basecasecost float, netcasecost float, netunitcost float, w1 decimal, w2 decimal, w3 decimal, w4 decimal, w5 decimal, w6 decimal, w7 decimal, w8 decimal, w9 decimal, w10 decimal, palletshipper varchar, msir varchar)");
+
+    done();
+    pg.end();
+
+  });
   res.sendFile(path.resolve('public/views/success.html'));
 });
-
-router.get('/1', function(req, res){
-  res.sendFile(path.resolve('public/uploads/1.json'));
-});
-
-router.get('/2', function(req, res){
-  res.sendFile(path.resolve('public/uploads/2.json'));
-});
-
-
-
-
-router.get('/clearFiles', function(req, res) {
-
-async.series([
-  function(callback){
-        var dataPath = ('/Users/lukasudstuen/softwareProjects/groceryComparer/public/uploads');
-        rimraf(dataPath, function(error){
-          console.log('Error: ', error);
-          callback();
-        });
-  },
-  function(callback) {
-    fs.mkdirsSync('/Users/lukasudstuen/softwareProjects/groceryComparer/public/uploads');
-  // res.sendStatus(200);
-},
-function(callback) {
-  res.sendStatus(200);
-  callback();
-}
-]);
-
-});
-
-
-
-
 
 
 module.exports = router;
