6c3bd6d254efc07e4e54979113c878f570351cb6
Lukas Udstuen <Lukas.Udstuen@gmail.com>
2017-05-22 19:25:35 -0500

    database correctly processes uploads

--- server/routes/base.js
+++ server/routes/base.js
@@ -1,14 +1,20 @@
 var express = require('express');
-var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
+var app = express();
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({extended:false}));
+
+
 var router = express.Router();
 var fs = require('fs-extra');
 var rimraf = require('rimraf');
 var pg = require('pg');
+// var client = new pg.Client();
+var client = new pg.Client();
+
 
 var async = require('async');
-
 
 if(process.env.DATABASE_URL !== undefined) {
      console.log('env connection string');
@@ -18,8 +24,6 @@ if(process.env.DATABASE_URL !== undefined) {
     // running locally, use our local database instead
     connectionString = 'postgres://localhost:5432/groc';
 }
-
-
 
 router.get('/', function (req, res){
   res.sendFile(path.resolve('public/views/index.html'));
@@ -48,22 +52,174 @@ router.get('/upload1view', function(req, res) {
   ]);
 });
 
-
 router.get('/upload2view', function(req, res){
   res.sendFile(path.resolve('public/views/upload2.html'));
 });
 
 router.get('/success', function(req, res){
+  console.log("We are here in results");
 
-  pg.connect(connectionString, function(err, client, done){
-    // This is the query that is creating all the problems. Will need to edit this. 
-    // client.query("CREATE TABLE supervaludeals (itemupc bigint, brand varchar, description varchar, pack integer, size varchar, mxdlwk varchar, mxdlamt float, mxdealprct decimal, basecasecost float, netcasecost float, netunitcost float, w1 decimal, w2 decimal, w3 decimal, w4 decimal, w5 decimal, w6 decimal, w7 decimal, w8 decimal, w9 decimal, w10 decimal, palletshipper varchar, msir varchar)");
 
-    done();
-    pg.end();
+  async.series([
+      function(callback) {
+          // do some stuff ...
+          pg.connect(connectionString, function(err, client, done){
+          var searchClient = ('DROP TABLE IF EXISTS goodgrocerdata, supervaludeals, results');
+          var query = client.query(searchClient);
+          query.on('end', function(){
+            done();
+            callback(null, 'one');
+          });
+          if(err){
+            console.log('logging an err: ', err);
+          }
+          });
+      },
+      function(callback) {
+          // do some more stuff ...
+          pg.connect(connectionString, function(err, client, done){
+          var searchClient = ('CREATE TABLE supervaludeals (itemupc bigint, brand varchar, description varchar, pack integer, size varchar, mxdlwk varchar, mxdlamt float, mxdealprct decimal, basecasecost float, netcasecost float, netunitcost float, w1 decimal, w2 decimal, w3 decimal, w4 decimal, w5 decimal, w6 decimal, w7 decimal, w8 decimal, w9 decimal, w10 decimal, palletshipper varchar, msir varchar)');
+          var query = client.query(searchClient);
+          query.on('end', function(){
+            done();
+            callback(null, 'two');
+          });
+          if(err){
+            console.log('logging an err: ', err);
+          }
+          });
+              // res.send('we are the beasts of the world');
+          // callback(null, 'two');
+      },
 
+
+      function(callback) {
+          // do some more stuff ...
+          pg.connect(connectionString, function(err, client, done){
+          var searchClient = ('CREATE TABLE goodgrocerdata (evo integer, descrptn varchar, addldsc varchar, upc bigint, dpt varchar, supervalu float, casevndr varchar, caseqty varchar, casecost varchar, unitcost float)');
+          var query = client.query(searchClient);
+          query.on('end', function(){
+            done();
+            callback(null, 'three');
+          });
+          if(err){
+            console.log('logging an err: ', err);
+          }
+          });
+              // res.send('we are the beasts of the world');
+          // callback(null, 'two');
+      },
+
+      function(callback) {
+          // do some more stuff ...
+          pg.connect(connectionString, function(err, client, done){
+          var searchClient = ("COPY supervaludeals FROM '/Users/lukasudstuen/softwareProjects/groceryComparer/public/uploads/supervaludata.csv' DELIMITER ',' CSV");
+          var query = client.query(searchClient);
+          query.on('end', function(){
+            done();
+            callback(null, 'four');
+          });
+          if(err){
+            console.log('logging an err: ', err);
+          }
+          });
+              // res.send('we are the beasts of the world');
+          // callback(null, 'two');
+      },
+      function(callback) {
+          // do some more stuff ...
+          pg.connect(connectionString, function(err, client, done){
+          var searchClient = ("COPY goodgrocerdata FROM '/Users/lukasudstuen/softwareProjects/groceryComparer/public/uploads/goodgrocerdata.csv' DELIMITER ',' CSV");
+          var query = client.query(searchClient);
+          query.on('end', function(){
+            done();
+            callback(null, 'five');
+          });
+          if(err){
+            console.log('logging an err: ', err);
+          }
+          });
+              // res.send('we are the beasts of the world');
+          // callback(null, 'two');
+      },
+
+      function(callback) {
+          // do some more stuff ...
+          pg.connect(connectionString, function(err, client, done){
+          var searchClient = ("SELECT * INTO results FROM supervaludeals, goodgrocerdata WHERE goodgrocerdata.upc = supervaludeals.itemupc ORDER BY mxdealprct DESC");
+          var query = client.query(searchClient);
+          query.on('end', function(){
+            done();
+            callback(null, 'six');
+          });
+          if(err){
+            console.log('logging an err: ', err);
+          }
+          });
+              // res.send('we are the beasts of the world');
+          // callback(null, 'two');
+      },
+
+
+
+
+  ],
+  // optional callback
+  function(err, results) {
+      // results is now equal to ['one', 'two']
+      res.send('we are the beasts of the world');
+        console.log("We have now received an error of: ", err);
+        console.log("With the results of: ", results);
   });
-  res.sendFile(path.resolve('public/views/success.html'));
+
+
+
+
+  //
+  // // connect to our database
+  // client.connect(function (err) {
+  //   if (err) throw err;
+  //
+  //   // execute a query on our database
+  //   client.query('DROP TABLE results', function (err, result) {
+  //     if (err) throw err;
+  //
+  //     // just print the result to the console
+  //     console.log(result.rows[0]); // outputs: { name: 'brianc' }
+  //
+  //     // disconnect the client
+  //     client.end(function (err) {
+  //       if (err) throw err;
+  //     });
+  //   });
+  // });
+  //
+
+// EVERYTHING ABOVE GOOD
+
+
+  // pg.connect(connectionString, function(err, client, done){
+    // This is the query that is creating all the problems. Will need to edit this.
+    // client.query("CREATE TABLE supervaludeals (itemupc bigint, brand varchar, description varchar, pack integer, size varchar, mxdlwk varchar, mxdlamt float, mxdealprct decimal, basecasecost float, netcasecost float, netunitcost float, w1 decimal, w2 decimal, w3 decimal, w4 decimal, w5 decimal, w6 decimal, w7 decimal, w8 decimal, w9 decimal, w10 decimal, palletshipper varchar, msir varchar)");
+    // if(err) {
+    //   done();
+    //   console.log(err);
+    //   return res.status(500).json({success: false, data: err});
+    // }
+    // pg.on( 'error', function(err) {
+    //   console.log('do nothing');
+    // });
+
+  //   console.log("We have made it to the drop table portion");
+  //   client.query ('DROP TABLE results;');
+  //   done();
+  //   pg.end();
+  //   res.sendFile(path.resolve('public/views/success.html'));
+  //
+  //
+  // });
+  // console.log('WE ARE DONE')
+  // res.sendFile(path.resolve('public/views/success.html'));
 });
 
 
